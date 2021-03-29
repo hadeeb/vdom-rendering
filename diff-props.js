958af04f14818c0c07bc83966f84029c6bc4d8a7
Hadeeb Farhan <hadeeb.kakkadavath@paytmmall.com>
2021-03-30 00:25:36 +0530

    Cleanup old props

--- diff-props.js
+++ diff-props.js
@@ -25,51 +25,67 @@ function diffProps(vnode, prevVNode) {
       // see utils.js L#17
       // For text nodes, domElement will be an instance of `Text`
       // see render.js L#46
-      domElement.data = newProps;
+      /**@type {Text} */ (domElement).data = newProps;
     } else {
-      // Iterate through props
-      Object.keys(newProps).forEach((key) => {
-        const newPropValue = newProps[key];
-        const prevPropValue = prevProps[key];
-
-        if (newPropValue !== prevPropValue) {
-          if (key === "key") {
-            // Do nothing
-          } else if (key === "ref") {
-            // Assign dom element to the ref
-            newPropValue.current = domElement;
-          } else if (key.startsWith("on")) {
-            // Events
-            // Converting key to lowercase to support both
-            // `onclick` and `onClick`
-            const event = key.toLowerCase().slice(2);
-            if (newPropValue) {
-              // An object is attached to the DOM element and
-              // event listeners are stored there.
-              if (!domElement.vnodeListeners) {
-                domElement.vnodeListeners = {};
-              }
-              domElement.vnodeListeners[event] = newPropValue;
-
-              if (!prevPropValue) {
-                // Using a common event handler to avoid repeatedly removing
-                // and adding event listeners
-                // eventProxy will invoke the currect listener using the
-                // value of `event.type`
-                domElement.addEventListener(event, eventProxy);
-              }
-            } else {
-              domElement.removeEventListener(event, eventProxy);
-            }
-          } else if (key in domElement) {
-            // DOM properties
-            domElement[key] = newPropValue;
-          } else {
-            // DOM attributes
-            domElement.setAttribute(key, newPropValue);
-          }
+      // Cleanup old props
+      Object.keys(prevProps).forEach((key) => {
+        if (key !== "key" && key !== "ref" && !(key in newProps)) {
+          setDOMProperty(domElement, key, null, prevProps[key]);
         }
       });
+      // Iterate through new props
+      Object.keys(newProps).forEach((key) => {
+        setDOMProperty(domElement, key, newProps[key], prevProps[key]);
+      });
+    }
+  }
+}
+/**
+ * Sets an attribute / eventlistener / property on a dom element
+ * @param {HTMLElement} domElement
+ * @param {string} key
+ * @param {any} newPropValue
+ * @param {any} prevPropValue
+ */
+function setDOMProperty(domElement, key, newPropValue, prevPropValue) {
+  if (newPropValue !== prevPropValue) {
+    if (key === "key") {
+      // Do nothing
+    } else if (key === "ref") {
+      // Assign dom element to the ref
+      /**@type {import("./hooks.js").RefHook<HTMLElement>} */ (newPropValue).current = domElement;
+    } else if (key.startsWith("on")) {
+      // Events
+      // An object is attached to the DOM element and
+      // event listeners are stored there.
+      if (!domElement.vnodeListeners) {
+        domElement.vnodeListeners = {};
+      }
+      // Converting key to lowercase to support both
+      // `onclick` and `onClick`
+      const event = key.toLowerCase().slice(2);
+      domElement.vnodeListeners[event] = newPropValue;
+      if (newPropValue) {
+        if (!prevPropValue) {
+          // Using a common event handler to avoid repeatedly removing
+          // and adding event listeners
+          // eventProxy will invoke the currect listener using the
+          // value of `event.type`
+          domElement.addEventListener(event, eventProxy);
+        }
+      } else {
+        domElement.removeEventListener(event, eventProxy);
+      }
+    } else if (key in domElement) {
+      // DOM properties
+      domElement[key] = newPropValue;
+    } else {
+      // DOM attributes
+      if (newPropValue) {
+        domElement.setAttribute(key, newPropValue);
+      } else {
+        domElement.removeAttribute(key);
+      }
     }
   }
 }
